8bbde29c82e03e37886716424f8efa0c25041984
liyicky <jason@liyicky.com>
2022-04-20 14:05:53 +0900

    Initial Commit

---
+++ Liy-Kanji/Liy_KanjiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Liy_KanjiApp.swift
+//  Liy-Kanji
+//
+//  Created by Jason Cheladyn on 2022/04/20.
+//
+
+import SwiftUI
+
+@main
+struct Liy_KanjiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
